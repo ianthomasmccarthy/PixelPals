60b1147e5e8704570508d52038e460835a384451
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-05-29 16:08:55 -0500

    adding pip freeze and celery references

--- ppserver/__init__.py
+++ ppserver/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from celery import Celery
+
 app = Flask(__name__)
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 from ppserver.pixel.controllers import pixel
 app.register_blueprint(pixel)
