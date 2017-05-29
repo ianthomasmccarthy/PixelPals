f6595ce3761dbc90af760359bf63e8e9dedef8f9
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-05-29 11:32:18 -0500

    adding url prefix

--- ppserver/pixel/controllers.py
+++ ppserver/pixel/controllers.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 pixel = Blueprint('pixel',
                   __name__,
+                  url_prefix='/pixel',
                   template_folder='templates/pixel')
 
 @pixel.route('/')
