1fce7a8f7290c0f79cc4feb1dda09b82649fbd52
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-06-03 13:34:55 -0500

    fixing timeiging

--- ppserver/__init__.py
+++ ppserver/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from celery import Celery
-
 app = Flask(__name__)
 app.config.from_object('config.Config')
-
+from ppserver.pixel.models import PPFeedback
+pp = PPFeedback()
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 from ppserver.pixel.controllers import pixel
 app.register_blueprint(pixel)
-from ppserver.pixel.models import PPFeedback
-pp = PPFeedback()
+
+
 
 
 import views
