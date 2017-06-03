68159e73c735ef3c49a868470566e4f1d98c5608
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-06-02 22:19:57 -0500

    update config

--- ppserver/__init__.py
+++ ppserver/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from celery import Celery
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
