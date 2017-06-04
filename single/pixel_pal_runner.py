028e7f38bc9c71d689db3a64c56b06638fa85a1c
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-06-03 21:45:37 -0500

    update

--- single/pixel_pal_runner.py
+++ single/pixel_pal_runner.py
@@ -32,20 +32,27 @@ class PixelPalRunner(object):
 
     def options(self):
         parser = self.get_parser()
-        parser.add_option('-h', '--host-name',   action='store', dest='hostname', help='Systems Hostname')
-        parser.add_option('-u', '--url-checkin', action='store', dest='url',      help='Url Checkin')
-        parser.add_option('-d', '--delay',       action='store', dest='delay',    help='delay between url checks' ),
-        parser.add_option('-l', '--log-file',    action='store', dest='log',    help='log location' ),
+        parser.add_option('-H', '--host-name',   action='store', dest='hostname', help='Systems Hostname')
+        parser.add_option('-U', '--url-checkin', action='store', dest='url',      help='Url Checkin')
+        parser.add_option('-D', '--delay',       action='store', dest='delay',    help='delay between url checks' ),
+        parser.add_option('-L', '--log-file',    action='store', dest='log',      help='log location' ),
         # retrive options
         (options, args) = parser.parse_args()
         # If we have command line options use instead of default value.
         self.hostname = options.hostname
         self.url      = options.url if options.url else self.url
         self.delay    = options.delay if options.delay else 20
-        self.log_file = options.log if options.log else None
+        self.log_file = options.log if options.log else self.log_file
 
     def check_url(self, url, hostname):
-        udata = requests.get(url)
+        try:
+            udata = requests.get(url)
+        except Exception as e:
+            self.logger.error(e)
+            if self.pp.state:
+                return True
+            else:
+                return False
         udata = str(udata).lower()
         if hostname.lower() in udata:
             return True
